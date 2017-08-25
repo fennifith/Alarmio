ada4d3a116469941efe9892b6bed2e4387bf4146
James Fenn <18jafenn90@gmail.com>
2017-08-25 13:07:22 -0400

    added stopwatch notification

--- app/src/main/java/james/alarmio/fragments/StopwatchFragment.java
+++ app/src/main/java/james/alarmio/fragments/StopwatchFragment.java
@@ -1,9 +1,19 @@
 package james.alarmio.fragments;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.view.GravityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +24,7 @@ import android.widget.TextView;
 
 import com.afollestad.aesthetic.Aesthetic;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -25,6 +36,11 @@ import james.alarmio.R;
 import james.alarmio.views.ProgressTextView;
 
 public class StopwatchFragment extends BaseFragment {
+
+    private static final int NOTIFICATION_ID = 247;
+    private static final String ACTION_RESET = "james.alarmio.StopwatchFragment.ACTION_RESET";
+    private static final String ACTION_TOGGLE = "james.alarmio.StopwatchFragment.ACTION_TOGGLE";
+    private static final String ACTION_LAP = "james.alarmio.StopwatchFragment.ACTION_LAP";
 
     private ImageView back;
     private ImageView reset;
@@ -40,8 +56,10 @@ public class StopwatchFragment extends BaseFragment {
     private long startTime, pauseTime, stopTime;
     private List<Long> laps;
     private long lastLapTime;
-    private long lastLapDiff;
     private boolean isRunning;
+    
+    private NotificationManager notificationManager;
+    private NotificationReceiver receiver;
 
     private int textColorPrimary;
 
@@ -59,6 +77,8 @@ public class StopwatchFragment extends BaseFragment {
         time = view.findViewById(R.id.time);
         lapsLayout = view.findViewById(R.id.laps);
 
+        notificationManager = (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        receiver = new NotificationReceiver(this);
         laps = new ArrayList<>();
 
         handler = new Handler();
@@ -67,8 +87,11 @@ public class StopwatchFragment extends BaseFragment {
             public void run() {
                 if (isRunning) {
                     long currentTime = System.currentTimeMillis() - startTime;
-                    time.setText(formatMillis(currentTime));
+                    String text = formatMillis(currentTime);
+                    time.setText(text);
                     time.setProgress(currentTime - (lastLapTime == 0 ? currentTime : lastLapTime));
+                    if (text.endsWith("00"))
+                        notificationManager.notify(NOTIFICATION_ID, getNotification(text.substring(0, text.length() - 3)));
                     handler.postDelayed(this, 10);
                 } else time.setText(formatMillis(startTime == 0 ? 0 : stopTime - startTime));
             }
@@ -77,80 +100,21 @@ public class StopwatchFragment extends BaseFragment {
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (isRunning) {
-                    stopTime = System.currentTimeMillis();
-                    isRunning = false;
-                }
-
-                startTime = 0;
-                pauseTime = 0;
-                handler.post(runnable);
-                laps.clear();
-                lapsLayout.removeAllViews();
-                lastLapTime = 0;
-                lastLapDiff = 0;
-                time.setMaxProgress(0);
-                time.setReferenceProgress(0);
-                reset.setVisibility(View.INVISIBLE);
-                lap.setVisibility(View.INVISIBLE);
-                share.setVisibility(View.GONE);
+                reset();
             }
         });
 
         toggle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                stopTime = System.currentTimeMillis();
-                isRunning = !isRunning;
-                if (isRunning) {
-                    if (startTime == 0)
-                        startTime = System.currentTimeMillis();
-                    else if (pauseTime != 0)
-                        startTime += System.currentTimeMillis() - pauseTime;
-                    handler.post(runnable);
-
-                    reset.setVisibility(View.INVISIBLE);
-                    lap.setVisibility(View.VISIBLE);
-                    share.setVisibility(View.GONE);
-                    toggle.setImageResource(R.drawable.ic_pause);
-                } else {
-                    pauseTime = System.currentTimeMillis();
-                    reset.setVisibility(View.VISIBLE);
-                    lap.setVisibility(View.GONE);
-                    share.setVisibility(View.VISIBLE);
-                    toggle.setImageResource(R.drawable.ic_play);
-                }
+                toggle();
             }
         });
 
         lap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                long lapTime = System.currentTimeMillis() - startTime;
-                long lapDiff = lapTime - lastLapTime;
-                laps.add(lapDiff);
-                if (lastLapTime == 0)
-                    time.setMaxProgress(lapDiff);
-                else time.setReferenceProgress(lapDiff);
-                lastLapTime = lapTime;
-
-                LinearLayout layout = new LinearLayout(getContext());
-
-                TextView number = new TextView(getContext());
-                number.setText(String.valueOf(laps.size()));
-                number.setTextColor(textColorPrimary);
-                layout.addView(number);
-
-                TextView time = new TextView(getContext());
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
-                layoutParams.weight = 1;
-                time.setLayoutParams(layoutParams);
-                time.setGravity(GravityCompat.END);
-                time.setText(formatMillis(lapTime));
-                time.setTextColor(textColorPrimary);
-                layout.addView(time);
-
-                lapsLayout.addView(layout, 0);
+                lap();
             }
         });
 
@@ -182,7 +146,86 @@ public class StopwatchFragment extends BaseFragment {
                 });
 
         handler.post(runnable);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_RESET);
+        filter.addAction(ACTION_TOGGLE);
+        filter.addAction(ACTION_LAP);
+        getContext().registerReceiver(receiver, filter);
+
         return view;
+    }
+
+    private void reset() {
+        if (isRunning)
+            toggle();
+
+        startTime = 0;
+        pauseTime = 0;
+        handler.post(runnable);
+        laps.clear();
+        lapsLayout.removeAllViews();
+        lastLapTime = 0;
+        time.setMaxProgress(0);
+        time.setReferenceProgress(0);
+        reset.setVisibility(View.INVISIBLE);
+        lap.setVisibility(View.INVISIBLE);
+        share.setVisibility(View.GONE);
+
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
+
+    private void toggle() {
+        stopTime = System.currentTimeMillis();
+        isRunning = !isRunning;
+        if (isRunning) {
+            if (startTime == 0)
+                startTime = System.currentTimeMillis();
+            else if (pauseTime != 0)
+                startTime += System.currentTimeMillis() - pauseTime;
+            handler.post(runnable);
+
+            reset.setVisibility(View.INVISIBLE);
+            lap.setVisibility(View.VISIBLE);
+            share.setVisibility(View.GONE);
+            toggle.setImageResource(R.drawable.ic_pause);
+        } else {
+            pauseTime = System.currentTimeMillis();
+            reset.setVisibility(View.VISIBLE);
+            lap.setVisibility(View.GONE);
+            share.setVisibility(View.VISIBLE);
+            toggle.setImageResource(R.drawable.ic_play);
+        }
+
+        notificationManager.notify(NOTIFICATION_ID, getNotification(formatMillis(System.currentTimeMillis() - startTime)));
+    }
+
+    private void lap() {
+        long lapTime = System.currentTimeMillis() - startTime;
+        long lapDiff = lapTime - lastLapTime;
+        laps.add(lapDiff);
+        if (lastLapTime == 0)
+            time.setMaxProgress(lapDiff);
+        else time.setReferenceProgress(lapDiff);
+        lastLapTime = lapTime;
+
+        LinearLayout layout = new LinearLayout(getContext());
+
+        TextView number = new TextView(getContext());
+        number.setText(String.valueOf(laps.size()));
+        number.setTextColor(textColorPrimary);
+        layout.addView(number);
+
+        TextView time = new TextView(getContext());
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.weight = 1;
+        time.setLayoutParams(layoutParams);
+        time.setGravity(GravityCompat.END);
+        time.setText(formatMillis(lapTime));
+        time.setTextColor(textColorPrimary);
+        layout.addView(time);
+
+        lapsLayout.addView(layout, 0);
     }
 
     @Override
@@ -190,6 +233,15 @@ public class StopwatchFragment extends BaseFragment {
         textColorPrimarySubscription.dispose();
         time.unsubscribe();
         super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        if (notificationManager != null && receiver != null) {
+            notificationManager.cancel(NOTIFICATION_ID);
+            getContext().unregisterReceiver(receiver);
+        }
+        super.onDestroy();
     }
 
     private String formatMillis(long millis) {
@@ -203,5 +255,54 @@ public class StopwatchFragment extends BaseFragment {
         else if (minutes > 0)
             return String.format(Locale.getDefault(), "%dm %02ds %02d", minutes, seconds, micros);
         else return String.format(Locale.getDefault(), "%ds %02d", seconds, micros);
+    }
+
+    private Notification getNotification(String time) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            notificationManager.createNotificationChannel(new NotificationChannel("stopwatch", getContext().getString(R.string.title_stopwatch), NotificationManager.IMPORTANCE_DEFAULT));
+
+        return new NotificationCompat.Builder(getContext(), "stopwatch")
+                .setSmallIcon(R.drawable.ic_stopwatch_notification)
+                .setContentTitle(getString(R.string.title_stopwatch))
+                .setContentText(time)
+                .setDeleteIntent(PendingIntent.getBroadcast(getContext(), 0, new Intent(ACTION_RESET), 0))
+                .addAction(
+                        isRunning ? R.drawable.ic_pause_notification : R.drawable.ic_play_notification,
+                        isRunning ? "Pause" : "Play",
+                        PendingIntent.getBroadcast(getContext(), 0, new Intent(ACTION_TOGGLE), 0)
+                )
+                .addAction(
+                        R.drawable.ic_lap_notification,
+                        "Lap",
+                        PendingIntent.getBroadcast(getContext(), 0, new Intent(ACTION_LAP), 0)
+                )
+                .build();
+    }
+
+    private static class NotificationReceiver extends BroadcastReceiver {
+
+        private WeakReference<StopwatchFragment> fragmentReference;
+
+        public NotificationReceiver(StopwatchFragment fragment) {
+            fragmentReference = new WeakReference<>(fragment);
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            StopwatchFragment fragment = fragmentReference.get();
+            if (intent != null && intent.getAction() != null && fragment != null) {
+                switch (intent.getAction()) {
+                    case ACTION_RESET:
+                        fragment.reset();
+                        break;
+                    case ACTION_TOGGLE:
+                        fragment.toggle();
+                        break;
+                    case ACTION_LAP:
+                        fragment.lap();
+                        break;
+                }
+            }
+        }
     }
 }
