4114410c57e94970247c6cf38b8e7af0e7dc501c
James Fenn <18jafenn90@gmail.com>
2018-06-21 22:43:44 -0400

    implemented crasher lib

--- app/src/main/java/james/alarmio/Alarmio.java
+++ app/src/main/java/james/alarmio/Alarmio.java
@@ -45,6 +45,7 @@ import james.alarmio.data.PreferenceData;
 import james.alarmio.data.TimerData;
 import james.alarmio.services.SleepReminderService;
 import james.alarmio.services.TimerService;
+import james.crasher.Crasher;
 
 public class Alarmio extends Application implements Player.EventListener {
 
@@ -74,6 +75,10 @@ public class Alarmio extends Application implements Player.EventListener {
     @Override
     public void onCreate() {
         super.onCreate();
+        Crasher crasher = new Crasher(this);
+        crasher.setEmail("dev@jfenn.me");
+        crasher.setColor(ContextCompat.getColor(this, R.color.colorNightPrimary));
+
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
         listeners = new ArrayList<>();
         alarms = new ArrayList<>();
