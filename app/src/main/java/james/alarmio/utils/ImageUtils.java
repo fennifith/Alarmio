c17b1b59a4439564115cd899410556876a87954f
James Fenn <18jafenn90@gmail.com>
2018-07-14 21:30:07 -0400

    i believe that i am a fish

--- app/src/main/java/james/alarmio/utils/ImageUtils.java
+++ app/src/main/java/james/alarmio/utils/ImageUtils.java
@@ -41,12 +41,26 @@ public class ImageUtils {
 
     public static void getBackgroundImage(ImageView imageView) {
         String backgroundUrl = PreferenceData.BACKGROUND_IMAGE.getValue(imageView.getContext());
+
         if (backgroundUrl != null && backgroundUrl.length() > 0) {
             if (backgroundUrl.startsWith("http"))
                 Glide.with(imageView.getContext()).load(backgroundUrl).into(imageView);
-            else if (backgroundUrl.contains("://"))
-                Glide.with(imageView.getContext()).load(Uri.parse(backgroundUrl)).into(imageView);
-            else Glide.with(imageView.getContext()).load(new File(backgroundUrl)).into(imageView);
+            else if (backgroundUrl.contains("://")) {
+                if (backgroundUrl.startsWith("content://")) {
+                    String path = Uri.parse(backgroundUrl).getLastPathSegment();
+                    if (path != null && path.contains(":"))
+                        path = "/storage/" + path.replaceFirst(":", "/");
+                    else path = Uri.parse(backgroundUrl).getPath();
+
+                    //      "a haiku"
+                    //I don't like storage
+                    //I'm sorry, poor developer
+                    //this is all my fault
+                    //          - james fenn, 2018
+
+                    Glide.with(imageView.getContext()).load(new File(path)).into(imageView);
+                } else Glide.with(imageView.getContext()).load(Uri.parse(backgroundUrl)).into(imageView);
+            } else Glide.with(imageView.getContext()).load(new File(backgroundUrl)).into(imageView);
         }
     }
 
